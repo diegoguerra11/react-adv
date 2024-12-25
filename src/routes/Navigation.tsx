97ea7f924f86d83c6330c0f320619b3392c20388
Diego Guerra <guerradiego92@gmail.com>
2024-12-25 10:24:21 -0500

    MIGRATE v17 TO v18

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,6 @@
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
   NavLink
 } from 'react-router-dom';
@@ -15,30 +15,23 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
+        {/* A <Routes> looks through its children <Route>s and renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about" element={<h1>About</h1>}/>
+          <Route path="/users" element={<h1>Users</h1>} />
+          <Route path="/" element={<h1>Home</h1>}/>
+        </Routes>
       </div>
     </Router>
   );
